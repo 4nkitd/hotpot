af746870d887afe702eac733a9a3ac80dcfff9db
Aerro <31719894+A-Aerro@users.noreply.github.com>
2017-10-04 19:38:38 +0530

    Added Documentation

--- hotpot.cmd
+++ hotpot.cmd
@@ -1,5 +1,27 @@
 @echo off
 
+REM MIT License
+REM
+REM Copyright (c) 2017 Aerro.Inc
+REM
+REM Permission is hereby granted, free of charge, to any person obtaining a copy
+REM of this software and associated documentation files (the "Software"), to deal
+REM in the Software without restriction, including without limitation the rights
+REM to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+REM copies of the Software, and to permit persons to whom the Software is
+REM furnished to do so, subject to the following conditions:
+REM
+REM The above copyright notice and this permission notice shall be included in all
+REM copies or substantial portions of the Software.
+REM
+REM THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+REM IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+REM FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+REM AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+REM LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+REM OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+REM SOFTWARE.
+
 
 :start
 cls
@@ -39,6 +61,8 @@ netsh wlan show hostednetwork
 pause
 goto start
 
+rem pre defined service no input required
+
 :activate
 echo .
 netsh wlan set hostednetwork mode=allow ssid=hotpot key=12345678
@@ -48,14 +72,21 @@ echo password : 12345678
 pause
 goto start
 
+rem stop all hotspot services
+
 :deactivate
 echo .
 netsh wlan stop hostednetwork
 pause
 goto start
 
+rem exit hte program
+
 :finish
 exit
 
+rem need help from online sources
+
 :help
-start "" https://aerro-a.blogspot.in
+start "" https://aerroweb.wordpress.com/hotpot/
+exit
